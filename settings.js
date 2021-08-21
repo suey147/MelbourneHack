00c7390b3bd3c422053cb5dc98afbdfeeca26ea6
suey <hlau0017@student.monash.edu>
2021-08-22 00:13:02 +0800

    .

--- settings.js
+++ settings.js
@@ -4,6 +4,17 @@ var select = document.querySelector(".timetable").addEventListener('click',funct
     var closesTime = e.target.closest(".cell");
     if(closesTime){
         closesTime.style.background = "rgb(30, 169, 39)";
+        console.log(closesTime);
     }
-    console.log(closesTime);
 })
+
+
+const blocking_site = document.getElementById("siteArray").value;
+const save = document.getElementById("enter");
+site=[];;
+save.addEventListener("click", () => {
+    site.push(blocking_site);
+    site.join("\n")
+    const blocked = blocking_site.split("\n").map(s => s.trim()).filter(Boolean);
+    chrome.storage.local.set({blocked});
+  });
